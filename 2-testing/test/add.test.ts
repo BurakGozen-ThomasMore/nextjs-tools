79e5852bd09292c404b0f054f0819dcb40def44e
BurakGozen <info@newcode.be>
2024-03-22 23:38:55 +0100

    feat: extra test case

--- 2-testing/test/add.test.ts
+++ 2-testing/test/add.test.ts
@@ -4,6 +4,10 @@ describe("adds 1 + 2 to equal 3", () => {
   it("should return 3", () => {
     expect(1 + 2).toBe(3);
   });
+
+  it("should return 4", () => {
+    expect(2 + 2).toBe(4);
+  });
 });
 
 test("adds 1 + 2 to equal 3", () => {
